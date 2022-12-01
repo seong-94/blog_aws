93f75f05f017295cb12826e549f35c27d2f26427
seong-94 <togussla@naver.com>
2022-12-01 21:19:09 +0900

    like comment testing

--- api/server.js
+++ api/server.js
@@ -3,17 +3,18 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import dotenv from "dotenv";
-import { v4 as uuidv4 } from "uuid";
-import mime from "mime-types";
+// import { v4 as uuidv4 } from "uuid";
+// import mime from "mime-types";
 import path from "path";
 import { fileURLToPath } from "url";
 import http from "http";
 import history from "connect-history-api-fallback";
 //routes
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
-import replyRoutes from "./routes/replys.js";
 
 dotenv.config();
 
@@ -22,25 +23,41 @@ const __dirname = path.dirname(__filename);
 const port = process.env.SERVER_PORT || "5000";
 const app = express();
 
+// const storage = multer.diskStorage({
+//   destinatio: (req, file, cb) => {
+//     cb(null, "image");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, `${uuidv4()}.${mime.extension(file.mimetype)}`);
+//   },
+// });
+
+// const upload = multer({
+//   storage,
+//   fileFilter: (req, file, cb) => {
+//     if (["image/jpeg", "image/jpg", "image/png"].includes(file.mimetype))
+//       cb(null, true);
+//     else cb(new Error("해당 파일의 형식을 지원하지 않습니다."), false);
+//   },
+//   limits: {
+//     fileSize: 1024 * 1024 * 5,
+//   },
+// });
+
 const storage = multer.diskStorage({
-  destinatio: (req, file, cb) => {
-    cb(null, "image");
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
   },
-  filename: (req, file, cb) => {
-    cb(null, `${uuidv4()}.${mime.extension(file.mimetype)}`);
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
   },
 });
 
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (["image/jpeg", "image/jpg", "image/png"].includes(file.mimetype))
-      cb(null, true);
-    else cb(new Error("해당 파일의 형식을 지원하지 않습니다."), false);
-  },
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
 });
 
 app.post("/upload", upload.single("file"), (req, res) => {
@@ -53,10 +70,11 @@ app.use(cookieParser());
 app.use(history());
 app.use(express.static(path.join(__dirname, "build")));
 
-app.use("/auth", authRoutes);
-app.use("/posts", postRoutes);
-app.use("/likes", likeRoutes);
-app.use("/reply", replyRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 app.use(
   cors({
